dc904db8658ac665e38c58f7582245bae3c186dc
Julianah <julianahbolarinwa@gmail.com>
2022-06-25 11:23:52 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var yourName = prompt("Hi what's your name? ");
+var height = prompt("And what's your height? No need to worry, you can give an estimate..");
+var nation = prompt("Lastly, what nationality are you? ");
+
+var msg = 'Your name is ' + yourName + ', <br/>Your height is ' + height + ', <br/>You are from ' + nation;
+
+document.write('<h2>' + msg + '</h2>');
